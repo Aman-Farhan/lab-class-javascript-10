c452b9362e8d31d378682acf5ce8883b69577973
Aman <amanfarhan2008@gmail.com>
2025-07-18 09:50:13 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+let biryani = 5;
+let pepsi = 1 ;
+
+for (let i = 0 ; i < biryani ; i++){
+    console.log("you have eat 5 plates of biryani");
+    for (let j = 0 ; j < pepsi ; j++){
+        console.log("you have drink pepsi");
+    }
+    for (let k = 0 ; k < pepsi ; k++){
+        console.log("you have eat pasty");
+    }
+    for (let z = 0 ; z < pepsi ; z++){
+        console.log("you have eat zinger");
+    }
+     for (let x = 0 ; x < pepsi ; x++){
+        console.log("you have drink pepsi" + "<br>"+ "<br>"+ "<br>");
+    }
+}
+for (let y = 0 ; y < pepsi ; y++){
+   console.log(" ab ambulance a gai hai ");
+}
+    
