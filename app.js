c59a449f2a2e45c4cd07b62fe5c4b26001bc192c
Aman <amanfarhan2008@gmail.com>
2025-07-18 23:26:30 +0500

    hgf

--- app.js
+++ app.js
@@ -84,10 +84,10 @@ slice()
 // console.log(firstName.slice(2));
 
 
-//  to UperCase :
+//  to UperCase 
 
 let firstName = prompt("Enter A Number");
-let uperName = firstName[0].toUpperCase()
+let uperName = firstName[0].toUpperCase();
 let lowerName = firstName.slice(1).toLowerCase();
 console.log(uperName + lowerName);
 
