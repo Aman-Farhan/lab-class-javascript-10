a1b3a660b6ae1685096b0429d8c762dba922f664
Aman <amanfarhan2008@gmail.com>
2025-07-18 10:29:38 +0500

    fifth code 10 > arrey

--- app.js
+++ app.js
@@ -30,9 +30,17 @@
 // }
 
 
-for(i = 0 ; i <= 100; i++){
-    if ( i % 2 === 0){
-        console.log("this is even number " + i);
-    }
-}
+// for(i = 0 ; i <= 100; i++){
+//     if ( i % 2 === 0){
+//         console.log("this is even number " + i);
+//     }
+// }
 
+
+let arrey = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+// let count = ;
+for (i= 0; i < arrey.length;  i++){
+    if(arrey[i]>10){
+    console.log(arrey[i]);
+}
+}
