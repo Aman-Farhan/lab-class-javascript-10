9e106afec4a4b308f1de778fbc1a4ea9b3f49736
Aman <amanfarhan2008@gmail.com>
2025-07-18 10:05:51 +0500

    second code

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
-let biryani = 5;
-let pepsi = 1 ;
+// let biryani = 5;
+// let pepsi = 1 ;
 
-for (let i = 0 ; i < biryani ; i++){
-    console.log("you have eat 5 plates of biryani");
-    for (let j = 0 ; j < pepsi ; j++){
-        console.log("you have drink pepsi");
-    }
-    for (let k = 0 ; k < pepsi ; k++){
-        console.log("you have eat pasty");
-    }
-    for (let z = 0 ; z < pepsi ; z++){
-        console.log("you have eat zinger");
-    }
-     for (let x = 0 ; x < pepsi ; x++){
-        console.log("you have drink pepsi" + "<br>"+ "<br>"+ "<br>");
-    }
-}
-for (let y = 0 ; y < pepsi ; y++){
-   console.log(" ab ambulance a gai hai ");
-}
+// for (let i = 1 ; i < biryani ; i++){
+//     console.log("you have eat 5 plates of biryani");
+//     for (let j = 1 ; j < pepsi ; j++){
+//         console.log("you have drink pepsi");
+//     }
+//     for (let k = 1 ; k < pepsi ; k++){
+//         console.log("you have eat pasty");
+//     }
+//     for (let z = 1 ; z < pepsi ; z++){
+//         console.log("you have eat zinger");
+//     }
+//      for (let x = 1 ; x < pepsi ; x++){
+//         console.log("you have drink pepsi" + "<br>"+ "<br>"+ "<br>");
+//     }
+// }
+// for (let y = 1 ; y < pepsi ; y++){
+//    console.log(" ab ambulance a gai hai ");
+// }
     
+ 
+for (let i = 0 ; i < 4; i++){
+    let row = "";
+    for(let j = 0 ; j < 7; j++){
+        row = row + " * ";
+    } console.log(row , i);
+
+}
