c660b29f769acf942a8b98a728b2dc438bd06f6f
Aman <amanfarhan2008@gmail.com>
2025-07-18 23:33:53 +0500

    sjdj

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ slice()
 let firstName = prompt("Enter A Number");
 let uperName = firstName[0].toUpperCase();
 let lowerName = firstName.slice(1).toLowerCase();
-console.log(uperName + lowerName)
+console.log(uperName + lowerName);
 
 // let firstName = prompt("enter a name");
 // let uperName = firstName[0].toUpperCase();
