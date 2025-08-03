e4b200ad204da9cfd4d3d4b12dc18f95ea4552dc
Aman <amanfarhan2008@gmail.com>
2025-08-03 23:58:52 +0500

    jjgsvd git push clear
    git add .

--- app.js
+++ app.js
@@ -38,7 +38,7 @@
 
 
 // let arrey = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-// let greaterCount = 
+// let greaterCount = ;
 // let lessCount = ;
 // for (i= 0; i < arrey.length;  i++){
 //     if(arrey[i]>10){
